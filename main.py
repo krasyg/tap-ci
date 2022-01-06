9fa8fcbd32ba9569aad6f672624b6565376d2eec
gabor.csuvik <gabor.csuvik@infinitelambda.com>
2022-01-06 13:56:30 +0100

    Add arguments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from sys import argv
+
+
 def add(n_1: int, n_2: int) -> int:
     return n_1 + n_2
 
@@ -7,4 +10,16 @@ def subtract(n_1: int, n_2: int) -> int:
 
 
 if __name__ == '__main__':
-    print(add(1, 2))
+    if len(argv) != 4:
+        print('Usage: number1 +/- number2')
+    else:
+        num_1 = int(argv[1])
+        num_2 = int(argv[3])
+        operator = argv[2]
+
+        if operator == '+':
+            print(add(num_1, num_2))
+        elif operator == '-':
+            print(subtract(num_1, num_2))
+        else:
+            print(f"Operator {operator} is not supported, use + or -.")
